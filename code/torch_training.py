6aeba37bffd718b59a4dae7729e837d9aa8c787b
Vendin Maxim <mv.vendin@gmail.com>
2022-03-29 13:23:02 +0300

    some changes in code

--- code/torch_training.py
+++ code/torch_training.py
@@ -1,32 +1,29 @@
 from pickle import load
-from tensorflow.keras.metrics import (FalseNegatives, FalsePositives,
-                                      TrueNegatives, TruePositives,
-                                      Recall, Precision)
 from torch.nn import Sequential, Linear, Sigmoid, ReLU, Module, BatchNorm1d, Dropout
-from torch import save
-from sklearn.metrics import f1_score, balanced_accuracy_score, accuracy_score
+from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import train_test_split
 from clearml import Task, Logger
 from torch.utils.data import Dataset, DataLoader
 
 import numpy as np
 import torch
-import torch.nn.functional as F
+
 
 # metrics
 def f1(t, p):
     tp = np.logical_and(t, p).astype(float).sum()
     fp = np.less(t, p).astype(float).sum()
     fn = np.less(p, t).astype(float).sum()
-    prec = tp/(tp+fp) #precision
-    rec = tp/(tp+fn)  #recall
-    return 2*((prec*rec)/(prec+rec))
+    prec = tp / (tp + fp)  # precision
+    rec = tp / (tp + fn)  # recall
+    return 2 * ((prec * rec) / (prec + rec))
 
 
 def accuracy(pred, target):
     return len(np.nonzero(pred == target)[0]) / (pred.shape[0] * pred.shape[1])
 
-#loader
+
+# loader
 class MoleculeDataset(Dataset):
     """Face Landmarks dataset."""
 
@@ -40,22 +37,24 @@ class MoleculeDataset(Dataset):
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
 
-#model
+
+# model
 class Chem(Module):
-    def __init__(self, in_f, n=1000):
+    def __init__(self, in_f, n=1000, k=500):
         super().__init__()
         self.in_f = in_f
 
         self.model_body = Sequential(
-            Linear(in_f, n),  # 1 2 3 4
-            # BatchNorm1d(n),  # 3 4
-            ReLU(inplace=True),
-            # Dropout(p=0.3, inplace=False),  # 4
-            # Linear(n, 500),  # 4
-            # BatchNorm1d(500),  # 4
-            # ReLU(inplace=True),  # 4
-            # Linear(500, 1)  # 4
-            Linear(n, 1)  # 2 3
+            Linear(in_f, n),  # 1 2 3 4 5 6
+            ReLU(inplace=True),  # 2 3 4 5 6
+            # BatchNormalization(trainable=True),  # 3  НЕ РАБОТАЕТ
+            # BatchNorm1d(n),  # 3
+            # Linear(n, 1),  # 2 3
+            Dropout(p=0.3, inplace=False),  # 6
+            Linear(n, k),  # 4 5 6
+            ReLU(inplace=True),  # 4 5 6
+            BatchNorm1d(k),  # 5 6
+            Linear(k, 1)  # 4 5 6
         )
         self.head = Sigmoid()
 
@@ -64,19 +63,64 @@ class Chem(Module):
         y = self.head(x)
         return y
 
-#logging
-def log(*args):
-    avg_acc, avg_loss, avg_f1, avg_ba, sample, epoch, dl, ind = args
+
+# logging
+def report_scalars_on_batch(*args):
+    avg_acc, avg_loss, avg_f1, avg_ba, sample, epoch, dl, batch_ind = args
     for name, metric in [avg_acc, avg_loss, avg_f1, avg_ba]:
         Logger.current_logger().report_scalar(
-                sample, name, iteration=(epoch * len(dl) + ind), value=metric)
+                sample, name, iteration=(epoch * len(dl) + batch_ind), value=metric)
+
+
+def report_scalars(*args):
+    avg_acc, avg_loss, avg_f1, avg_ba, sample, epoch = args
+    for name, metric in [avg_acc, avg_loss, avg_f1, avg_ba]:
+        if all([x is not None for x in [name, metric]]):
+            Logger.current_logger().report_scalar(
+                sample, name, iteration=epoch, value=metric)
+
+
+def report_test_plots(avg_m, avg_f1, avg_ba, model_name, predictions, target, series):
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    from sklearn.metrics import ConfusionMatrixDisplay
+    from sklearn.metrics import confusion_matrix
+    # reporting pd table
+    df = pd.DataFrame(
+        {
+            "acc": [avg_m],
+            "f1": [avg_f1],
+            "bac": [avg_ba],
+        },
+        index=[model_name],
+    )
+    df.index.name = "model_name"
+    Logger.current_logger().report_table(
+        "table pd",
+        model_name,
+        iteration=0,
+        table_plot=df
+    )
+
+    # reporting confusion matrix
+    labels = ["Reconstructed", "Decoy"]
+    cm = confusion_matrix(target, predictions)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap=plt.cm.Blues)
+
+    Logger.current_logger().report_matplotlib_figure(
+        title="Confusion matrix",
+        series=series,
+        iteration=0,
+        figure=plt,
+    )
 
 
 if __name__ == "__main__":
-
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    task = Task.init(project_name="filter_model", task_name="NN with two hidden layears batch norm and dropout")
-    with open('./fps_pkls/united_lfp_6_4096_230322.pickle', 'rb') as f:
+    model_name = "logistic regression"
+    task = Task.init(project_name="filter_model", task_name=model_name)
+    with open("./fps_pkls/united_lfp_6_4096_230322.pickle", 'rb') as f:
         data = load(f)
 
     X = np.stack([l[0][0] for l in list(data.values())])
@@ -99,6 +143,7 @@ if __name__ == "__main__":
 
     in_f = x_train.shape[1]
     model = Chem(in_f)
+    Logger.current_logger().report_text(str(model))
 
     epochs = 100
     opt = torch.optim.Adam(model.parameters(), lr=1e-4)
@@ -108,17 +153,9 @@ if __name__ == "__main__":
     log_freq = 100
     model.to(device)
 
-    best_loss = 2 ** 64
-    patience = 0
-    max_patience = 1
-    epoch = 0
-    best_model = model
+    for epoch in range(1, epochs + 1):
 
-    #train
-    while epoch < epochs:  # patience < max_patience and
-        epoch += 1
-
-        avg_acc, avg_f1, avg_loss, avg_balanced_accuracy_score = 0, 0, 0, 0
+        train_avg_acc, train_avg_bal_acc, train_avg_f1, train_avg_loss = 0, 0, 0, 0
         for batch_ind, (x, y) in enumerate(train_dl):
             x, y = x.to(device), y.to(device)
 
@@ -128,43 +165,46 @@ if __name__ == "__main__":
             y_pred = model(x)
 
             loss = criterion(y_pred, y)
-            avg_loss += loss
+            train_avg_loss += loss
 
             pred_transformed = y_pred > threshold
             m = accuracy(pred_transformed.cpu().numpy(), y.cpu().numpy())
-            avg_acc += m
+            train_avg_acc += m
 
             f = f1(y.cpu().numpy(), pred_transformed.cpu().numpy())
-            avg_f1 += f
+            train_avg_f1 += f
 
             ba = balanced_accuracy_score(y.cpu().numpy(), pred_transformed.cpu().numpy())
-            avg_balanced_accuracy_score += ba
+            train_avg_bal_acc += ba
 
             loss.backward()
             opt.step()
             opt.zero_grad()
             if batch_ind % log_freq == 0 and batch_ind > 0:
-                # посчитать метрики на traininig set ACC, BA, F1
-                print('[%d/%d][%d/%d] Loss: %.4f ' % (epoch, epochs, batch_ind,
-                                                      len(train_dl),
-                                                      loss.item()))
+                report_scalars_on_batch(("acc", train_avg_acc / batch_ind),
+                                        ("loss", train_avg_loss.item() / batch_ind),
+                                        ("f1", train_avg_f1 / batch_ind),
+                                        ("bac", train_avg_bal_acc / batch_ind),
+                                         "train", epoch, train_dl, batch_ind)
+                print('[%d/%d][%d/%d] Loss: %.4f' % (epoch, epochs, batch_ind,
+                                                     len(train_dl),
+                                                     loss.item()))
 
-        avg_acc = avg_acc / len(train_dl)
-        avg_loss = avg_loss / len(train_dl)
-        avg_f1 = avg_f1 / len(train_dl)
-        avg_ba = avg_balanced_accuracy_score / len(train_dl)
+        train_avg_acc /= len(train_dl)
+        train_avg_loss /= len(train_dl)
+        train_avg_f1 /= len(train_dl)
+        train_avg_bal_acc /= len(train_dl)
 
-        log(("acc", avg_acc),
-            ("loss", avg_loss.item()),
-            ("f1", avg_f1),
-            ("bac", avg_ba),
-            "train", epoch, train_dl, batch_ind)
+        report_scalars(("acc_train", train_avg_acc),
+                       ("loss_train", train_avg_loss.item()),
+                       ("f1_train", train_avg_f1),
+                       ("bac_train", train_avg_bal_acc),
+                       "train", epoch)
 
         # Evaluation
-        best_model.eval()
         with torch.no_grad():
 
-            avg_acc, avg_f1, avg_loss, avg_balanced_accuracy_score = 0, 0, 0, 0
+            valid_avg_acc, valid_avg_f1, valid_avg_loss, valid_avg_bal_acc = 0, 0, 0, 0
             for x, y in valid_dl:
                 x, y = x.to(device), y.to(device)
 
@@ -174,43 +214,44 @@ if __name__ == "__main__":
                 y_pred = model(x)
 
                 loss = criterion(y_pred, y)
-                avg_loss += loss
+                valid_avg_loss += loss
 
                 pred_transformed = y_pred > threshold
                 m = accuracy(pred_transformed.cpu().numpy(), y.cpu().numpy())
-                avg_acc += m
+                valid_avg_acc += m
+
                 f = f1(y.cpu().numpy(), pred_transformed.cpu().numpy())
-                avg_f1 += f
+                valid_avg_f1 += f
 
                 ba = balanced_accuracy_score(y.cpu().numpy(), pred_transformed.cpu().numpy())
-                avg_balanced_accuracy_score += ba
+                valid_avg_bal_acc += ba
 
-            avg_acc = avg_acc / len(valid_dl)
-            avg_loss = avg_loss / len(valid_dl)
-            avg_f1 = avg_f1 / len(valid_dl)
-            avg_ba = avg_balanced_accuracy_score / len(valid_dl)
+            valid_avg_acc /= len(valid_dl)
+            valid_avg_loss /= len(valid_dl)
+            valid_avg_f1 /= len(valid_dl)
+            valid_avg_bal_acc /= len(valid_dl)
 
-            if avg_loss.item() < best_loss:
-                best_loss = avg_loss.item()
-                best_model = model
-            # else:
-            #     patience += 1
-
-            log(("acc", avg_acc),
-                ("loss", avg_loss.item()),
-                ("f1", avg_f1),
-                ("bac", avg_ba),
-                "valid", epoch, valid_dl, batch_ind)
-            print(
-                '[{:d}/{:d}] Accuracy {:.3f} F1-score {:.3f}  BA {:.3f}| Loss: {:.4f}\n'.format(epoch, epochs, avg_acc,
-                                                                                                avg_f1, avg_ba,
-                                                                                                avg_loss.item()))
+            # reporting metric scalars on validation set
+            report_scalars(("acc_valid", valid_avg_acc),
+                           ("loss_valid", valid_avg_loss.item()),
+                           ("f1_valid", valid_avg_f1),
+                           ("bac_valid", valid_avg_bal_acc),
+                           "validation", epoch)
+            # reporting loss on test and validation set
+            report_scalars((None, None), (None, None),
+                           ("loss_train", train_avg_loss.item()),
+                           ("loss_valid", valid_avg_loss.item()),
+                           "train_and_validation_loss", epoch)
+            print('[{:d}/{:d}] Accuracy {:.3f} F1-score {:.3f}  BA {:.3f}| Loss: {:.4f}\n'.format(epoch, epochs,
+                                                                                                  valid_avg_acc,
+                                                                                                  valid_avg_f1,
+                                                                                                  valid_avg_bal_acc,
+                                                                                                  valid_avg_loss.item()))
 
     test_ds = MoleculeDataset(x_test, y_test)
     test_dl = DataLoader(test_ds, batch_size=1024, shuffle=False, drop_last=True)
 
     # test
-    # model.to(device)
     predictions = []
     target = []
 
@@ -226,8 +267,10 @@ if __name__ == "__main__":
             pred = model(x)
             predictions.append(pred)
             pred_transformed = pred > threshold
-            m = accuracy(pred_transformed.cpu().numpy(), y.cpu().numpy())  #
+
+            m = accuracy(pred_transformed.cpu().numpy(), y.cpu().numpy())
             avg_m += m
+
             f = f1(y.cpu().numpy(), pred_transformed.cpu().numpy())
             avg_f1 += f
 
@@ -237,6 +280,13 @@ if __name__ == "__main__":
         avg_m = avg_m / len(test_dl)
         avg_f1 = avg_f1 / len(test_dl)
         avg_ba = avg_balanced_accuracy_score / len(test_dl)
-        print('Accuracy {:.3f} F1-score {:.3f}  BA {:.3f}\n'.format(avg_m, avg_f1, avg_ba))
 
-    torch.save(best_model, 'nn_with_2_hidden_layers_batchnorm_anddroput.pth')
+        report_test_plots(avg_m, avg_f1, avg_ba, model_name,
+                          np.array([int(x.item()) for x in pred_transformed]),
+                          np.array([int(x.item()) for x in target[-1]]), "On last batch")
+
+        report_test_plots(avg_m, avg_f1, avg_ba, model_name,
+                          np.array([1 if x.item() > threshold else 0 for tens in predictions for x in tens]),
+                          np.array([int(x.item()) for arr in target for x in arr]), "All")
+
+        print('Accuracy {:.3f} F1-score {:.3f}  BA {:.3f}\n'.format(avg_m, avg_f1, avg_ba))
